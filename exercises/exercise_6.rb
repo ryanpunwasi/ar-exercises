d86567d76eae2981ef4cf8e564d6dcc4d1580e4b
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-26 00:39:56 -0400

    Create many employees for stores 1 and 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,22 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+class Store
+  has_many :employees
+  
+end
+
+class Employee
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jon", last_name: "Snow", hourly_rate: 40)
+@store1.employees.create(first_name: "Jaime", last_name: "Lannister", hourly_rate: 100)
+@store1.employees.create(first_name: "Sansa", last_name: "Stark", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Otto", last_name: "Hightower", hourly_rate: 40)
+@store2.employees.create(first_name: "Brynden", last_name: "Rivers", hourly_rate: 90)
+@store2.employees.create(first_name: "Bran", last_name: "Stark", hourly_rate: 20)
+@store2.employees.create(first_name: "Roose", last_name: "Bolton", hourly_rate: 40)
