b60f699167a920d2a33687e619a7b32090bc838f
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-26 00:37:50 -0400

    Implement validation for the Store and Empployee model

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,30 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store_id, presence: true
+end
+
+class Store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 } 
+  validate :carry_mens_or_womens
+
+  def carry_mens_or_womens
+    if !mens_apparel && !womens_apparel
+      errors.add(:carry_mens_or_womens, ": Stores must carry at least one of the men's or women's apparel")
+    end
+  end
+end
+
+store_name = gets.chomp!
+store = Store.create(name: store_name)
+
+store.errors.full_messages.each do | message |
+  puts message
+end
+
